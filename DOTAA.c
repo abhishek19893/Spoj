78d4bc5c6bebe836d16d01be8d272794fc12dd92
abhishek19893 <abhishek19893@users.noreply.github.com>
2014-04-11 19:27:12 +0530

    Create DOTAA.c

---
+++ DOTAA.c
@@ -0,0 +1,35 @@
+//abhishek gupta
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	int t, m, n, d;
+	scanf("%d", &t);
+	while(t--)
+	{
+		register int i, count=0;
+		scanf("%d %d %d", &n, &m, &d);
+		int h;
+		for(i=0; i<n; i++)
+		{
+			scanf("%d", &h);
+			while(count<=m)
+			{
+				h-=d;
+				if(h>0)
+					count++;
+				else
+					break;
+			}
+		}
+		if(count>=m)
+			puts("YES");
+		else
+			puts("NO");
+			
+	}
+	
+	return 0;
+}
+
